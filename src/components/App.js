5d1814310e914b6912d5510a244d9aaceff67be4
hanlacey <hnnhlcy@gmail.com>
2021-06-21 10:43:14 +0100

    update router

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,20 @@
 import '../styles/App.css'
 import AllLines from './AllLines';
+import Header from './Header';
+import Description from './Description';
+import Widget from './Widget';
 import { Router } from '@reach/router'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Router>
-        <AllLines path="/" />
-      </Router>
+        <Widget path="/travel" />
+        <Widget path="/travel/:line_id" />
 
-    </div>
+      </Router>
+    </div >
   );
 }
 
